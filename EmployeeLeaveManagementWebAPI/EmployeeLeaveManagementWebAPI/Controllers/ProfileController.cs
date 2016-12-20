e06b2fae63bde8a6cd80dc98d9fe16a6790837ef
DESKTOP-IEOF3HT\E7440 <alekhya224@users.noreply.github.com>
2016-12-20 11:55:00 +0530

    LMS Missing files

---
+++ EmployeeLeaveManagementWebAPI/EmployeeLeaveManagementWebAPI/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using LMS_WebAPI_Domain;
+using LMS_WebAPI_ServiceHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeLeaveManagementWebAPI.Controllers
+{
+
+    public class ProfileController : ApiController
+    {
+        private const string LocalLoginProvider = "Local";
+        private UserManagement userManager = new UserManagement();
+        [AllowAnonymous]
+        [HttpGet]
+        public EmployeeDetailsModel GetUserProfileDetails(int empId)
+        {
+            try
+            {
+                var empData = userManager.GetUserProfileDetails(empId);
+                if (null != empData)
+                {
+                    return empData;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+                //throw;
+            }
+        }
+    }
+}
