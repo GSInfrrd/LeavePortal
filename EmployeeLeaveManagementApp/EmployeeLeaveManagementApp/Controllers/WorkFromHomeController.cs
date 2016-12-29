a91473fe8f7d2981ced4d4f08bd6b2d1af575190
AnuNithinUkken <anu@infrrd.ai>
2016-12-29 10:50:23 +0530

    New Files are added

---
+++ EmployeeLeaveManagementApp/EmployeeLeaveManagementApp/Controllers/WorkFromHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeLeaveManagementApp.Controllers
+{
+    public class WorkFromHomeController : Controller
+    {
+        // GET: WorkFromHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
