36d02e8e9851237c83dcd70c13349c5927061eff
Alekhya <alekhya224@users.noreply.github.com>
2017-01-09 16:27:00 +0530

    Master Data scripts

---
+++ DatabaseBackups/Scripts/MasterDataValue_AddNew_01092017.sql
@@ -0,0 +1,13 @@
+Use LeaveManagementSystem
+Go
+
+SET IDENTITY_INSERT [dbo].[MasterDataValue]  ON
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (27, 7, N'.Net')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (28, 7, N'Java')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (29, 7, N'CSS')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (30, 7, N'SQL')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (31, 7, N'Php')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (32, 7, N'SAP')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (33, 7, N'Oracle')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (34, 7, N'Html')
+SET IDENTITY_INSERT [dbo].[MasterDataType] OFF
