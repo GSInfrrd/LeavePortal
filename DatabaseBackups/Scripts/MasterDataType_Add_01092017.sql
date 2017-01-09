8f88e4530debbb9ae9c5e64075c7374ce45a07c5
Alekhya <alekhya224@users.noreply.github.com>
2017-01-09 16:26:19 +0530

    Skills :Master Data Type & values

---
+++ DatabaseBackups/Scripts/MasterDataType_Add_01092017.sql
@@ -0,0 +1,7 @@
+Use LeaveManagementSystem
+Go
+
+SET IDENTITY_INSERT [dbo].[MasterDataType] ON
+INSERT INTO [dbo].[MasterDataType] ([Id], [Type]) VALUES (7, N'Skills')
+SET IDENTITY_INSERT [dbo].[MasterDataType] OFF
+
