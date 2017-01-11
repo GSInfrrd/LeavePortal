7e1547d0168459a3f29c35f1010fb07cb5212af1
Alekhya <alekhya224@users.noreply.github.com>
2017-01-11 16:57:31 +0530

    EMployee Detail Script

---
+++ DatabaseBackups/Scripts/Jan/6.EmployeeDetail_AddColumns.sql
@@ -0,0 +1,13 @@
+use LeaveManagementSystem
+go
+
+
+Alter Table EmployeeDetails
+Add FacebookLink varchar(max) 
+
+Alter Table EmployeeDetails
+Add TwitterLink varchar(max) 
+
+
+Alter Table EmployeeDetails
+Add GooglePlusLink varchar(max) 
