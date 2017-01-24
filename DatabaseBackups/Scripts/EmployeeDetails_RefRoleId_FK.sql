89d6ea9ecd1e4ff37180a2d39997f4d2759b17e1
Alekhya <alekhya224@users.noreply.github.com>
2017-01-24 15:17:45 +0530

    EmployeeDetails FK Script

--- DatabaseBackups/Scripts/EmployeeDetails_RefRoleId_FK.sql
+++
@@ -1,9 +0,0 @@
-Use LeaveManagementSystem
-Go
-
-ALTER TABLE [dbo].[EmployeeDetails]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeDetails_MasterDataValue2] FOREIGN KEY([RefRoleId])
-REFERENCES [dbo].[MasterDataValue] ([Id])
-GO
-
-ALTER TABLE [dbo].[EmployeeDetails] CHECK CONSTRAINT [FK_EmployeeDetails_MasterDataValue2]
-GO
