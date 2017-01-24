b10a40d0c32837817d2e30564e8ca3210509aba1
E7440 <E7440@DESKTOP-IEOF3HT>
2017-01-24 23:18:54 +0530

    scriipts to run

---
+++ DatabaseBackups/ScriptsToRun-24Jan/AlterLeaveTransaction.sql
@@ -0,0 +1,5 @@
+ Alter table [LeaveManagementSystem].[dbo].[EmployeeLeaveTransaction]
+  Alter column [FromDate] [date] null
+
+  Alter table [LeaveManagementSystem].[dbo].[EmployeeLeaveTransaction]
+  Alter column [ToDate] [date] null
