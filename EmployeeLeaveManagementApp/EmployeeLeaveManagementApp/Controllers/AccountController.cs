3079a2362e328977c721459806fbedbc1ff957d9
Mahak Jain <mahak@infrrd.ai>
2017-01-25 14:23:36 +0530

    forgot password link hide

--- EmployeeLeaveManagementApp/EmployeeLeaveManagementApp/Controllers/AccountController.cs
+++ EmployeeLeaveManagementApp/EmployeeLeaveManagementApp/Controllers/AccountController.cs
@@ -94,7 +94,7 @@ namespace EmployeeLeaveManagementApp.Controllers
             return View("Login");
         }
 
-        public async Task<ActionResult> Profile()
+        public async new Task<ActionResult> Profile()
         {
             try
             {
