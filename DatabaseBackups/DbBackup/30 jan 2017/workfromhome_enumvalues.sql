cd4baecaa5cef8064153ec76ca73cb6718692589
vikash-m <vikash.mahala@raremile.com>
2017-01-30 17:25:27 +0530

    work from home enums

---
+++ DatabaseBackups/DbBackup/30 jan 2017/workfromhome_enumvalues.sql
@@ -0,0 +1,3 @@
+insert into [LeaveManagementSystem].[dbo].[EmailTemplateMaster] values('\App_Data\EmailTemplates\WorkFromHome.cshtml')
+
+insert into [LeaveManagementSystem].[dbo].[EmailTemplateMapping] values('WorkFromHome',8)
