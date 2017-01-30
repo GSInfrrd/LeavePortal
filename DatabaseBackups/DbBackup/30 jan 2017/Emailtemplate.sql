cbdd107b8d64ec240d160f7f577273a625250d17
vikash-m <vikash.mahala@raremile.com>
2017-01-30 12:59:13 +0530

    Email Template Mappings Update : Script

---
+++ DatabaseBackups/DbBackup/30 jan 2017/Emailtemplate.sql
@@ -0,0 +1,3 @@
+insert into [LeaveManagementSystem].[dbo].[EmailTemplateMaster] values('\App_Data\EmailTemplates\RewardLeave.cshtml')
+
+insert into [LeaveManagementSystem].[dbo].[EmailTemplateMapping] values('RewardLeave',7)
