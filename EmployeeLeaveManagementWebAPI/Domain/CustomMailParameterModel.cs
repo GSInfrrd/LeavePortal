71dfbffb465c28ccff0ec4433f21ad8df94a4f43
vikash-m <vikash.mahala@raremile.com>
2017-01-30 12:40:36 +0530

    Email changes

---
+++ EmployeeLeaveManagementWebAPI/Domain/CustomMailParameterModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_WebAPI_Domain
+{
+    public class CustomMailParameterModel
+    {
+        public string MailFrom { get; set; }
+
+        public string MailTo { get; set; }
+
+        public string Subject { get; set; }
+
+        public int NubmerOfWorkingDays { get; set; }
+
+        public DateTime FromDate { get; set; }
+
+        public DateTime ToDate { get; set; }
+
+        public string LeaveComments { get; set; }
+
+    }
+}
