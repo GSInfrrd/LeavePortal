d1d8829c328c6a6e708242900887e81b2ffc0f49
Alekhya <alekhya224@users.noreply.github.com>
2017-01-31 10:01:45 +0530

    MasterData Value Script

--- DatabaseBackups/DbBackup/30 jan 2017/5.Insert_LeaveType_MasterDataValue.sql
+++ DatabaseBackups/DbBackup/30 jan 2017/5.Insert_LeaveType_MasterDataValue.sql
@@ -2,3 +2,4 @@ Use LeaveManagementSystem
 go
 
 INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (66, 3, N'Reward Leave')
+INSERT INTO [dbo].[MasterDataValue] ([Id], [RefMasterType], [Value]) VALUES (67, 3, N'Earned Leave')
