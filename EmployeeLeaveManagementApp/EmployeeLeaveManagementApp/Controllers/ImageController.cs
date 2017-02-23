d91fba24aa5d98216d2ddb850175aec530d95ac2
vikash-m <vikash.mahala@raremile.com>
2017-02-23 15:08:01 +0530

    Crop image changes

--- EmployeeLeaveManagementApp/EmployeeLeaveManagementApp/Controllers/ImageController.cs
+++ EmployeeLeaveManagementApp/EmployeeLeaveManagementApp/Controllers/ImageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LMS_WebAPP_Utils;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
@@ -14,33 +15,43 @@ namespace EmployeeLeaveManagementApp.Controllers
         [HttpPost]
         public virtual ActionResult CropImage(string imagePath, int? cropPointX, int? cropPointY, int? imageCropWidth, int? imageCropHeight)
         {
-            if (string.IsNullOrEmpty(imagePath) || !cropPointX.HasValue || !cropPointY.HasValue || !imageCropWidth.HasValue || !imageCropHeight.HasValue)
-            {
-                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
-            }
-
-           // string strimg = imagePath.Substring(imagePath.IndexOf(',')+1);
-           // byte[] b = Convert.FromBase64String(strimg);
-           // string strOriginal = System.Text.Encoding.UTF8.GetString(b);
-            byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
-            byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
-
-            string tempFolderName = Server.MapPath(ConfigurationManager.AppSettings["EmployeeImagePath"]);
-            string fileName = Path.GetFileName(imagePath);
-            string tempcropImagepath = Server.MapPath(ConfigurationManager.AppSettings["EmployeeImagePath"]) + "\\" +fileName;
-
+            Logger.Info("Entering in ImageController APP CropImage method");
             try
             {
-                FileHelper.SaveFile(croppedImage, Path.Combine(tempFolderName, fileName));
+                if (string.IsNullOrEmpty(imagePath) || !cropPointX.HasValue || !cropPointY.HasValue || !imageCropWidth.HasValue || !imageCropHeight.HasValue)
+                {
+                    return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+                }
+
+                // string strimg = imagePath.Substring(imagePath.IndexOf(',')+1);
+                // byte[] b = Convert.FromBase64String(strimg);
+                // string strOriginal = System.Text.Encoding.UTF8.GetString(b);
+                byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
+                byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
+
+                string tempFolderName = Server.MapPath(ConfigurationManager.AppSettings["EmployeeImagePath"]);
+                string fileName = Path.GetFileName(imagePath);
+                string tempcropImagepath = Server.MapPath(ConfigurationManager.AppSettings["EmployeeImagePath"]) + "\\" + fileName;
+
+                try
+                {
+                    FileHelper.SaveFile(croppedImage, Path.Combine(tempFolderName, fileName));
+                }
+                catch (Exception ex)
+                {
+                    //Log an error     
+                    return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+                }
+
+                string photoPath = string.Concat("/", ConfigurationManager.AppSettings["Image.TempFolderName"], "/", fileName);
+                Logger.Info("Successfully exiting from ImageController APP CropImage method");
+                return Json(new { photoPath = photoPath, tempcropImagepath = tempcropImagepath }, JsonRequestBehavior.AllowGet);
             }
             catch (Exception ex)
             {
-                //Log an error     
-                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+                Logger.Error("Error at ImageController APP CropImage method.", ex);
+                return View("Error");
             }
-
-            string photoPath = string.Concat("/", ConfigurationManager.AppSettings["Image.TempFolderName"], "/", fileName);
-            return Json(new { photoPath = photoPath , tempcropImagepath = tempcropImagepath }, JsonRequestBehavior.AllowGet);
         }
     }
 }
